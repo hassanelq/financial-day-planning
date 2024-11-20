6205a390b6cfeaa00313426d43b21cf680ae7ba4
hassan_elq <hassanelqadi2@gmail.com>
2024-11-20 16:07:46 +0100

    Font

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,12 @@
 import "./globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Arial", "sans-serif"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.variable} antialiased`}>{children}</body>
     </html>
   );
 }
