1a4b002cbf33f8e4ffc0686cd40aab4c77f9ec2d
hassan_elq <hassanelqadi2@gmail.com>
2024-11-21 18:38:35 +0100

    feat: add Open Graph and Twitter metadata for improved SEO and sharing

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Montserrat } from "next/font/google";
+import ogImage from "../public/ogImage.png";
 
 const montserrat = Montserrat({
   weight: ["300", "400", "500", "700"],
@@ -12,6 +13,42 @@ export const metadata = {
   title: "Financial Day - 4th Edition",
   description:
     "Les Nouveaux Horizons de La Finance : Tendances, Technologies et Opportunités de Marché",
+  metadataBase: new URL("https://finday4.vercel.app/"),
+
+  openGraph: {
+    type: "website",
+    url: "https://finday4.vercel.app/",
+    title: "Financial Day - 4th Edition",
+    description:
+      "Les Nouveaux Horizons de La Finance : Tendances, Technologies et Opportunités de Marché",
+    images: [
+      {
+        url: ogImage.src,
+        width: 1200,
+        height: 630,
+        alt: "Financial Day - 4th Edition",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Financial Day - 4th Edition",
+    description:
+      "Les Nouveaux Horizons de La Finance : Tendances, Technologies et Opportunités de Marché",
+    images: [
+      {
+        url: ogImage.src,
+        width: 1200,
+        height: 630,
+        alt: "Financial Day - 4th Edition",
+      },
+    ],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({ children }) {
